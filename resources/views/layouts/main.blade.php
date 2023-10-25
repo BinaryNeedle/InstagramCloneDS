1285c3139aef423c72e497907c85bf596c97039d
jTnqr <juliuspanjul25@gmail.com>
2023-10-25 10:52:05 +0700

    called tailwind using vite

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-
+    @vite('resources/css/app.css')
 
     {{-- Fontawesome --}}
     <script src="https://kit.fontawesome.com/67db78fea9.js" crossorigin="anonymous"></script>
