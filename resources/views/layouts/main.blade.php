91e56a0d129e250c4e702ee6edd714f2248c8ffb
MRaihanZ <mraihanzhafran.14@gmail.com>
2023-11-01 04:39:49 +0700

    fixing navbar,sidebar and adding layout in for post

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="dark">
 
 <head>
     <meta charset="UTF-8">
@@ -14,11 +14,16 @@
     {{-- Google Font --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
     {{-- Custom CSS Here --}}
     <link rel="stylesheet" href="{{ URL::asset('/src/css/layouts/main.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('/src/css/partials/navbars.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://unpkg.com/flexmasonry/dist/flexmasonry.css">
+    <script src="https://unpkg.com/flexmasonry/dist/flexmasonry.js"></script>
     @yield('css')
     <style>
         @font-face {
@@ -36,14 +41,23 @@
 
 <body>
     @include('partials.navbar2')
-
-    <div class="container wrapper">
-        @yield('content')
+    {{-- manual overide the background color for darkmode --}}
+    <div class="p-4 sm:ml-64 dark:bg-[#111828]">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+            <div class="container wrapper">
+                @yield('content')
+            </div>
+        </div>
     </div>
 
     @include('partials.footers')
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+    <script>
+        FlexMasonry.init('.grid');
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
     @yield('script')
 </body>
 
