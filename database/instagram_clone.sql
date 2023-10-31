662e78c21eb37565072b4781799a553be5d672c9
Ahmad Zaky <humami539@gmail.com>
2023-10-31 14:29:08 +0700

    db

--- database/instagram_clone.sql
+++ database/instagram_clone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 24, 2023 at 05:26 AM
+-- Generation Time: Oct 28, 2023 at 03:54 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
