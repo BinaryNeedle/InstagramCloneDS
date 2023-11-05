07bb33a679aaac7a7f0d1ec86dee9fc2b141a0b3
MRaihanZ <mraihanzhafran.14@gmail.com>
2023-11-06 05:51:48 +0700

    before the merge login and main

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -2,48 +2,61 @@
 @section('title', 'Login to Instantgram')
 @section('css')
 @section('content')
-<div class="flex justify-center items-center h-screen">
-    <div class="w-full max-w-md">
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <h1 class="text-2xl font-bold mb-6">Login</h1>
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+    <div class="flex justify-center items-center h-screen">
+        <div class="w-full max-w-md">
+            <div class="dark:bg-[#1f2937] shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <h1 class="text-2xl font-bold mb-6 dark:text-white text-center">Login | Instantgram</h1>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
 
-                <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="username_or_email">
-                        Username or Email
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username_or_email" type="text" name="username_or_email" value="{{ old('username_or_email') }}" required autofocus>
-                    @error('username_or_email')
-                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                    @enderror
-                </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 font-bold mb-2 dark:text-white" for="username_or_email">
+                            Username or Email
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username_or_email" type="text" name="username_or_email"
+                            value="{{ old('username_or_email') }}" required autofocus>
+                        @error('username_or_email')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="password">
-                        Password
-                    </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" required>
-                    @error('password')
-                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                    @enderror
-                </div>
+                    <div class="mb-4">
+                        <label class="block text-gray-700 font-bold mb-2 dark:text-white" for="password">
+                            Password
+                        </label>
+                        <input
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password" type="password" name="password" required>
+                        @error('password')
+                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Login
-                    </button>
+                    <div class="flex flex-col items-center justify-between">
+                        <button
+                            class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit">
+                            Login
+                        </button>
+                        <p class="dark:text-white my-5">Didn't have account yet?</p>
+                        <a href="/regsiter"
+                            class="bg-blue-500 w-full text-center hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            Register
+                        </a>
 
-                    @if (Route::has('password.request'))
-                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
-                        Forgot Password?
-                    </a>
-                    @endif
-                </div>
-            </form>
+                        @if (Route::has('password.request'))
+                            <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                                href="{{ route('password.request') }}">
+                                Forgot Password?
+                            </a>
+                        @endif
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 @endsection
 @section('script')
 
