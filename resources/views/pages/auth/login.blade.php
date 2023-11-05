61481d103a959e14519351d1dd8639ea29eb5218
MRaihanZ <mraihanzhafran.14@gmail.com>
2023-11-06 06:01:32 +0700

    Resolved confilcts in login.blade.php

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -33,19 +33,20 @@
                             <p class="text-red-500 text-xs italic">{{ $message }}</p>
                         @enderror
                     </div>
-
-<<<<<<< HEAD
                     <div class="flex flex-col items-center justify-between">
                         <button
                             class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                             type="submit">
                             Login
                         </button>
-                        <p class="dark:text-white my-5">Didn't have account yet?</p>
-                        <a href="/regsiter"
-                            class="bg-blue-500 w-full text-center hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                            Register
-                        </a>
+
+                        @if (Route::has('register'))
+                            <p class="dark:text-white my-5">Didn't have account yet?</p>
+                            <a class="bg-blue-500 w-full text-center hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                href="{{ route('register') }}">
+                                Register
+                            </a>
+                        @endif
 
                         @if (Route::has('password.request'))
                             <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
@@ -56,31 +57,8 @@
                     </div>
                 </form>
             </div>
-=======
-
-
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Login
-                    </button>
-
-                    @if (Route::has('register'))
-                    <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" href="{{ route('register') }}">
-                        Register
-                    </a>
-                    @endif
-
-                    @if (Route::has('password.request'))
-                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('password.request') }}">
-                        Forgot Password?
-                    </a>
-                    @endif
-                </div>
-            </form>
->>>>>>> 7d0aa329e8312805b9390529863dbc9c020e23e3
         </div>
-    </div>
-@endsection
-@section('script')
+    @endsection
+    @section('script')
 
-@endsection
+    @endsection
