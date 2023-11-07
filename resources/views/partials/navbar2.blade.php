111a45723b16c0c63aeaa2a1b779808f54f9705a
jTnqr <juliuspanjul25@gmail.com>
2023-11-08 00:27:23 +0700

    fixed logout button text alignment

--- resources/views/partials/navbar2.blade.php
+++ resources/views/partials/navbar2.blade.php
@@ -49,9 +49,8 @@
                             <li>
                                 <form method="POST" action="{{ route('logout') }}">
                                     @csrf
-                                    <button type="submit" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Logout</button>
+                                    <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Logout</button>
                                 </form>
-                                <!-- <a href="" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a> -->
                             </li>
                         </ul>
                     </div>
