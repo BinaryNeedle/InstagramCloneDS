1514bbdbeea4e8d66157d1bd9f1f9a5a0abde128
MRaihanZ <mraihanzhafran.14@gmail.com>
2023-11-08 13:35:32 +0700

    after merging from zaky

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,17 +13,11 @@
     @unless (Route::is(['login', 'register']))
         @include('partials.navbar')
     @endunless
-
-
-<<<<<<< HEAD
     <main id="main"
-        class="{{ Route::is(['login', 'register']) ? 'p-4 w-full dark:bg-[#111828] h-screen' : 'pt-14 md:ml-64 dark:bg-[#111828] h-screen overflow-y-hidden' }}">
+        class="{{ Route::is(['login', 'register']) ? 'p-4 w-full dark:bg-[#111828] h-screen' : 'pt-14 sm:ml-64 dark:bg-[#111828] h-screen overflow-y-hidden' }}">
         <section
             class="{{ Route::is(['login', 'register']) ? 'border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 h-full' : 'h-full' }}">
-=======
-    <main id="main" class="{{ Route::is(['login', 'register']) ? 'p-4 w-full dark:bg-[#111828] h-screen' : 'pt-14 sm:ml-64 dark:bg-[#111828] h-screen overflow-y-hidden' }}">
-        <section class="{{ Route::is(['login', 'register']) ? 'border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 h-full' : 'h-full' }}">
->>>>>>> bc9571ec7ef1da1d32a7f1f66bfbdf49a5b629f4
+            >>>>>>> bc9571ec7ef1da1d32a7f1f66bfbdf49a5b629f4
             <section class="container mx-auto wrapper h-full">
                 @yield('content')
             </section>
