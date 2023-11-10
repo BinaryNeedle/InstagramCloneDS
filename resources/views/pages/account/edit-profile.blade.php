25ca3836e420dfc3a1e4c884dac247e9386949bd
MRaihanZ <mraihanzhafran.14@gmail.com>
2023-11-11 00:30:14 +0700

    editing edit page and setting page

--- resources/views/pages/account/edit-profile.blade.php
+++ resources/views/pages/account/edit-profile.blade.php
@@ -7,13 +7,14 @@
         <div id="content" class="w-full max-w-md my-auto">
             <div class="dark:bg-[#1f2937] shadow-md rounded px-8 pt-6 pb-8">
                 <img src="{{ URL::asset('assets/imgs/logo-exported.png') }}" class="h-32 mx-auto" alt="Instantgram Logo" />
-                <h2 class="dark:text-white text-2xl font-bold mb-6 text-center">Update Setting Account</h2>
+                <h2 class="dark:text-white text-2xl font-bold mb-6 text-center">Edit Account</h2>
                 <form method="POST" action="{{ route('profileSetting', ['username' => '@' . auth()->user()->username]) }}">
                     @csrf
                     <div class="mb-4">
                         <label for="username" class="block text-gray-700 font-bold mb-2 dark:text-white">Username</label>
-                        <input id="username" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('username') border-red-500 @enderror" name="username" value="{{ old('username') }}" required
-                            autocomplete="username" autofocus>
+                        <input id="username" type="text"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('username') border-red-500 @enderror"
+                            name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
                         @error('username')
                             <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
                         @enderror
@@ -21,8 +22,9 @@
 
                     <div class="mb-4">
                         <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">Name</label>
-                        <input id="name" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('name') border-red-500 @enderror" name="name" value="{{ old('name') }}" required autocomplete="name"
-                            autofocus>
+                        <input id="name" type="text"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('name') border-red-500 @enderror"
+                            name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
                         @error('name')
                             <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
                         @enderror
@@ -30,15 +32,26 @@
 
                     <div class="mb-4">
                         <label for="bio" class="block text-gray-700 font-bold mb-2 dark:text-white">Bio</label>
-                        <textarea id="bio" type="text" rows="10" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('bio') border-red-500 @enderror" name="bio" value="{{ old('bio') }}" required autocomplete="bio"
-                            autofocus></textarea>
+                        <textarea id="bio" type="text" rows="10"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('bio') border-red-500 @enderror"
+                            name="bio" value="{{ old('bio') }}" required autocomplete="bio" autofocus></textarea>
+                        @error('bio')
+                            <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="bio" class="block text-gray-700 font-bold mb-2 dark:text-white">Profile
+                            Image</label>
+                        <input class="dark:text-white" name="profileImage" type="file">
                         @error('bio')
                             <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
                         @enderror
                     </div>
 
                     <div class="flex flex-col items-center justify-between">
-                        <button type="submit" class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        <button type="submit"
+                            class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                             Update
                         </button>
                     </div>
--- resources/views/pages/account/setting.blade.php
+++ resources/views/pages/account/setting.blade.php
@@ -11,34 +11,27 @@
                 <form method="POST" action="{{ route('profileSetting', ['username' => '@' . auth()->user()->username]) }}">
                     @csrf
                     <div class="mb-4">
-                        <label for="username" class="block text-gray-700 font-bold mb-2 dark:text-white">Username</label>
-                        <input id="username" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('username') border-red-500 @enderror" name="username" value="{{ old('username') }}" required
-                            autocomplete="username" autofocus>
-                        @error('username')
+                        <label for="email" class="block text-gray-700 font-bold mb-2 dark:text-white">Email</label>
+                        <input id="email" type="email"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('email') border-red-500 @enderror"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
                             <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
                         @enderror
                     </div>
 
                     <div class="mb-4">
-                        <label for="name" class="block text-gray-700 font-bold mb-2 dark:text-white">Name</label>
-                        <input id="name" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('name') border-red-500 @enderror" name="name" value="{{ old('name') }}" required autocomplete="name"
-                            autofocus>
-                        @error('name')
+                        <label for="password" class="block text-gray-700 font-bold mb-2 dark:text-white">Password</label>
+                        <input id="password" type="password"
+                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('password') border-red-500 @enderror"
+                            name="password" value="{{ old('password') }}" required autocomplete="password" autofocus>
+                        @error('password')
                             <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
                         @enderror
                     </div>
-
-                    <div class="mb-4">
-                        <label for="bio" class="block text-gray-700 font-bold mb-2 dark:text-white">Bio</label>
-                        <textarea id="bio" type="text" rows="10" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @error('bio') border-red-500 @enderror" name="bio" value="{{ old('bio') }}" required autocomplete="bio"
-                            autofocus></textarea>
-                        @error('bio')
-                            <p class="text-red-500 text-xs italic mt-4 pl-1">{{ $message }}</p>
-                        @enderror
-                    </div>
-
                     <div class="flex flex-col items-center justify-between">
-                        <button type="submit" class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        <button type="submit"
+                            class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                             Update
                         </button>
                     </div>
